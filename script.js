835ce17159662ade8e2e224be88f425cdf5376ce
Paito Anderson <pj.paito@gmail.com>
2018-08-11 14:09:35 -0400

    Nav menu

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-$(document).ready(() => {
 
-    // // Background Cross-Fading
+// Nav Menu
+$( document ).ready(function() {
+    var menuTrigger = $('.sidebar-trigger');
+    var menuTriggerPosition = $('.sidebar-trigger').position();
+    var nav = $('.sidebar-nav');
+    var navWidth = nav.outerWidth();
 
-    // let images = [
-    //     "images/backgrounds/background1.jpg",
-    //     "images/backgrounds/background2.jpg",
-    //     "images/backgrounds/background3.jpg"
-    // ];
+    if (!nav.hasClass('is--active')) {
+        menuTrigger.click(function() {
+        nav.toggleClass('is--active');
+        });
+    } else {
+    $('body').click(function() {
+        nav.removeClass('is--active');
+        });
+    }
 
-    // let header = document.querySelector("header");
-
-    // // Interval Timer in Seconds
-    // let seconds = 5;
-      
-    // function backgroundSequence() {
-    //     window.clearTimeout();
-    //     let k = 0;
-    //     for (i = 0; i < images.length; i++) {
-    //         let newHeader = "url(" + images[i] + ")";
-    //         setTimeout(function(){ 
-    //             document.querySelector("header").style.backgroundImage = newHeader;
-    //         if ((k + 1) === images.length) { setTimeout(function() { backgroundSequence() }, (seconds * 1000))} else { k++; }			
-    //         }, (seconds * 1000) * i)	
-    //     }
-    // }
-    // backgroundSequence();
-
-    // Header background color on scroll
-    // window.onscroll = function() {changeBackground()};
-
-    // function changeBackground() {
-    //     $('#fixed-header').animate({backgroundColor: 'eggshell'}, 'slow');
-    //     $('#fixed-header').animate({color: 'black'}, 'fast');
-    // }
-    
-
+    nav.css({'top' : menuTriggerPosition.top});
 });
-
-
