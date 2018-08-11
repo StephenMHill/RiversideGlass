a13b625387cb8c6dce9b4d9f1654c9ac5666c95c
Paito Anderson <pj.paito@gmail.com>
2018-08-11 14:31:22 -0400

    Hamburger x

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ $( document ).ready(function() {
     var menuTrigger = $('.sidebar-trigger');
     var menuTriggerPosition = $('.sidebar-trigger').position();
     var nav = $('.sidebar-nav');
-    var navWidth = nav.outerWidth();
 
     if (!nav.hasClass('is--active')) {
         menuTrigger.click(function() {
-        nav.toggleClass('is--active');
+            nav.toggleClass('is--active');
+            menuTrigger.toggleClass('is--active');
         });
     } else {
-    $('body').click(function() {
-        nav.removeClass('is--active');
+        $('body').click(function() {
+            nav.removeClass('is--active');
+            menuTrigger.toggleClass('is--active');
         });
     }
 
