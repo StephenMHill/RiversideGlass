f7bad08feac1efed7adddd9be558238d933a48cc
StephenMHill <xhillx99@gmail.com>
2018-08-11 13:16:04 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(() => {
+
+    // // Background Cross-Fading
+
+    // let images = [
+    //     "images/backgrounds/background1.jpg",
+    //     "images/backgrounds/background2.jpg",
+    //     "images/backgrounds/background3.jpg"
+    // ];
+
+    // let header = document.querySelector("header");
+
+    // // Interval Timer in Seconds
+    // let seconds = 5;
+      
+    // function backgroundSequence() {
+    //     window.clearTimeout();
+    //     let k = 0;
+    //     for (i = 0; i < images.length; i++) {
+    //         let newHeader = "url(" + images[i] + ")";
+    //         setTimeout(function(){ 
+    //             document.querySelector("header").style.backgroundImage = newHeader;
+    //         if ((k + 1) === images.length) { setTimeout(function() { backgroundSequence() }, (seconds * 1000))} else { k++; }			
+    //         }, (seconds * 1000) * i)	
+    //     }
+    // }
+    // backgroundSequence();
+
+    // Header background color on scroll
+    // window.onscroll = function() {changeBackground()};
+
+    // function changeBackground() {
+    //     $('#fixed-header').animate({backgroundColor: 'eggshell'}, 'slow');
+    //     $('#fixed-header').animate({color: 'black'}, 'fast');
+    // }
+    
+
+});
+
+
